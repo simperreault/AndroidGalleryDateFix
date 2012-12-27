a2071d8195476972446e72189bcccf7d4ff0bb4e
Dalto <dalto.perreault@gmail.com>
2012-12-27 12:22:56 -0500

    Added the job on a background job with a progress bar

--- GalleryFix/src/org/madduck/galleryfix/MainActivity.java
+++ GalleryFix/src/org/madduck/galleryfix/MainActivity.java
@@ -9,16 +9,20 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -43,17 +47,21 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode,
+			final Intent data) {
 		// Check which request we're responding to
 		if (requestCode == 1) {
 			// Make sure the request was successful
 			if (resultCode == RESULT_OK) {
+
 				fixFolder(data.getDataString().replaceFirst("file:///", "")
 						.replaceFirst("folder:///", ""));
+
 			}
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	private boolean fixFolder(String folderPath) {
 
 		File folder = new File(folderPath);
@@ -67,58 +75,92 @@ public class MainActivity extends Activity implements OnClickListener {
 				}
 			}
 		}
-
-		Collections.sort(files);
-
-		Log.i("toto", files.toString());
-
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(new Date());
-
-		Log.i("toto", files.size() + "");
-
-		for (String filename : files) {
-
-			try {
-				ExifInterface i = new ExifInterface(filename);
-
-				// 2012:12:28 19:09:31
-				// 2012:12:26 19:54:42
-				SimpleDateFormat dateFormatter = new SimpleDateFormat(
-						"yyyy:MM:dd");
-				SimpleDateFormat timeFormatter = new SimpleDateFormat(
-						"HH:mm:ss");
-
-				Date date = new Date(calendar.getTimeInMillis());
-				
-				i.setAttribute(ExifInterface.TAG_DATETIME,
-						dateFormatter.format(date) + " " + timeFormatter.format(date));
-				i.setAttribute(ExifInterface.TAG_GPS_DATESTAMP, dateFormatter.format(date));
-				i.setAttribute(ExifInterface.TAG_GPS_TIMESTAMP, timeFormatter.format(date));
-
-				// i.setAttribute(ExifInterface.TAG_DATETIME,
-				// "2020:12:28 19:09:51");
-
-				Log.i("toto",
-						filename + " : "
-								+ i.getAttribute(ExifInterface.TAG_DATETIME));
-
-				i.saveAttributes();
-
-			} catch (IOException e) {
-				Log.e("toto", "Throw");
-				e.printStackTrace();
-			}
-
-			calendar.add(Calendar.DATE, -1);
-		}
-
-		sendBroadcast(new Intent(
-				Intent.ACTION_MEDIA_MOUNTED,
-				Uri.parse("file://" + Environment.getExternalStorageDirectory())));
-
-		Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
+		
+		Button btnFix = (Button) findViewById(R.id.btn_fix_date);
+		btnFix.setVisibility(8);
+		
+		ProgressBar bar = (ProgressBar) findViewById(R.id.progress_fix);
+		bar.setVisibility(0);
+		
+		FixFolderTask task = new FixFolderTask(this, bar, btnFix);
+		task.execute(files);
 
 		return true;
+	}
+
+	@SuppressLint("SimpleDateFormat")
+	private class FixFolderTask extends AsyncTask<List<String>, Integer, Long> {
+
+		private Context context;
+		private ProgressBar progressBar;
+		private Button btnFix;
+
+		public FixFolderTask(Context context, ProgressBar progressBar, Button btnFix) {
+			this.context = context;
+			this.progressBar = progressBar;
+			this.btnFix = btnFix;
+		}
+
+		protected Long doInBackground(List<String>... allFiles) {
+			
+			List<String> files = allFiles[0];
+			Collections.sort(files);
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(new Date());
+			
+			progressBar.setMax(files.size());
+
+			int i = 1;
+			for (String filename : files) {
+				publishProgress(i);
+				++i;
+				
+				
+				try {
+					ExifInterface exif = new ExifInterface(filename);
+
+					SimpleDateFormat dateFormatter = new SimpleDateFormat(
+							"yyyy:MM:dd");
+					SimpleDateFormat timeFormatter = new SimpleDateFormat(
+							"HH:mm:ss");
+
+					Date date = new Date(calendar.getTimeInMillis());
+
+					exif.setAttribute(
+							ExifInterface.TAG_DATETIME,
+							dateFormatter.format(date) + " "
+									+ timeFormatter.format(date));
+
+					exif.setAttribute(ExifInterface.TAG_GPS_DATESTAMP, "");
+					exif.setAttribute(ExifInterface.TAG_GPS_TIMESTAMP, "");
+
+					exif.saveAttributes();
+
+				} catch (IOException e) {
+					Log.e("toto", "Throw");
+					e.printStackTrace();
+				}
+
+				calendar.add(Calendar.DATE, -1);
+			}
+
+			sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,
+					Uri.parse("file://"
+							+ Environment.getExternalStorageDirectory())));
+
+			return 0L;
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+			progressBar.setProgress(progress[0]);
+		}
+
+		protected void onPostExecute(Long result) {
+			Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show();
+			
+			progressBar.setVisibility(8);
+			btnFix.setVisibility(0);
+		}
 	}
 }
